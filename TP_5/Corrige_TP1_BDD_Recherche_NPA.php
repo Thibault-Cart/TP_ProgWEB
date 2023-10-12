0dd734e298e86df8eb20f74965c07d83a9a52ebc
Thibault <49438882+Thibault-Cart@users.noreply.github.com>
2023-10-12 14:09:01 +0200

    2 3 modif

--- TP_5/Corrige_TP1_BDD_Recherche_NPA.php
+++ TP_5/Corrige_TP1_BDD_Recherche_NPA.php
@@ -1,6 +1,75 @@
 <?php
 
+function XML_creator($rows)
+{
+  // Récupération des données de la base de données
+  if (count($rows) > 0) {
 
+    // Créez un nouveau XML document
+    $xml_header = '<?xml version="1.0" encoding="UTF-8"?><LIST_Localite></LIST_Localite>';
+    $xml = new SimpleXMLElement($xml_header);
+
+
+
+    // Convertissez et ajoutez les données ligne par ligne au XML
+    foreach ($rows as $row) {
+
+      //ajout d'un noeud enfant au noeud racine
+      $node1 = $xml->addChild('Localite');
+      //ajout d'un noeud  pour chaque valeur
+      $node1->addChild('npa_id', $row[0]);
+      $node1->addChild('npa_localite', $row[2]);
+      $node1->addChild('npa_code', $row[1]);
+    }
+
+    $test = $xml->asXML('localite.xml');
+
+    if ($test == true) {
+      echo "Le fichier XML a été créé avec succès";
+    }
+  }
+}
+
+
+function JSON_creator($rows)
+{
+  // Si aucun résultat n'a été trouvé, on génère une erreur.
+  if (count($rows) == 0) {
+    throw new Exception("Aucune localité ne correspond à votre recherche<br/>");
+  }
+
+  // creation array vide
+  $result = array();
+
+
+  // Parcours les résultats trouvés pour affichage
+  foreach ($rows as $row) {
+
+    // Exercice 03
+    // Voir si affichage du lien
+    if (isset($_POST['avecLien'])) {
+      // Construction du lien
+      $lien = '<a href="https://www.google.ch/?q=' . $row->npa_localite . '">Voir dans Google</a>';
+    } else {
+      $lien = '';
+    }
+    // array temporaire prend comme valeur un localite
+    $temp = array('id' => "{$row->npa_id}", 'localite' => "{$row->npa_localite}", 'code' => "{$row->npa_code}", 'lien' => "{$lien}");
+    // array temporaire est ajouter dans le array result
+    array_push($result, $temp);
+    // reinitialisation du array temporaire
+    $temp = array();
+  }
+  // creation du fichier json avec comme param le nom et le array result converti en json
+  $creation = file_put_contents('localite.json', json_encode($result));
+
+  // si la creation du fichier json est false alors afficher un message d'erreur
+  if ($creation == false) {
+    echo "Probleme lors de la creation du fichier json";
+  } else {
+    echo "Fichier json creer avec succes";
+  }
+}
 
 // Définit les constantes de connexions.
 define('DB_HOST', 'localhost');
@@ -26,37 +95,10 @@ if (isset($_POST['rechercher'])) {
     if ($rec->num_rows == 0) {
       throw new Exception("Aucune localité ne correspond à votre recherche<br/>");
     }
-
-
+    // Récupération des résultats (sous format d'un tableau d'objet)
     $rows = $rec->fetch_all();
 
-    // Récupération des données de la base de données
-    if (count($rows) > 0) {
-
-      // Créez un nouveau XML document
-      $xml_header = '<?xml version="1.0" encoding="UTF-8"?><LIST_Localite></LIST_Localite>';
-      $xml = new SimpleXMLElement($xml_header);
-
-
-
-      // Convertissez et ajoutez les données ligne par ligne au XML
-      foreach ($rows as $row) {
-
-
-        $node1 = $xml->addChild('Localite');
-
-        $node1->addChild('npa_id', $row[0]);
-        $node1->addChild('npa_localite', $row[2]);
-        $node1->addChild('npa_code', $row[1]);
-      }
-
-      $test = $xml->asXML('localite.xml');
-
-      if ($test == true) {
-        echo "Le fichier XML a été créé avec succès";
-      }
-    }
-
+    XML_creator($rows);
 
     // Affichage de l'erreur si elle a été trouvée.
   } catch (Exception $e) {
@@ -65,7 +107,7 @@ if (isset($_POST['rechercher'])) {
 }
 
 
-// Exercice 02 (et exercice 03)
+
 
 // Tester si la recherche a été déclenchée
 if (isset($_POST['rechercher_ex2'])) {
@@ -89,41 +131,10 @@ if (isset($_POST['rechercher_ex2'])) {
     $stmt->execute();
     // Récupération des résultats (sous format d'un tableau d'objet)
     $rows = $stmt->fetchAll(PDO::FETCH_CLASS);
-    // Si aucun résultat n'a été trouvé, on génère une erreur.
-    if (count($rows) == 0) {
-      throw new Exception("Aucune localité ne correspond à votre recherche<br/>");
-    }
-    $result=array();
+
+    JSON_creator($rows);
 
 
-    // Parcours les résultats trouvés pour affichage
-    foreach ($rows as $row) {
-       
-      // Exercice 03
-      // Voir si affichage du lien
-      if (isset($_POST['avecLien'])) {
-        // Construction du lien
-        $lien = '<a href="https://www.google.ch/?q=' . $row->npa_localite . '">Voir dans Google</a>';
-      } else {
-        $lien = '';
-      }
-      $temp=array('id'=>"{$row->npa_id}",'localite'=>"{$row->npa_localite}",'code'=>"{$row->npa_code}",'lien'=>"{$lien}");
-
-      array_push($result,$temp);
-      $temp=array();
-      // Affichage de la ligne résultat avec le lien (ou non)
-     // echo ("$row->npa_localite $row->npa_code $lien<br/>");
-    }
-   
-    $creation=file_put_contents('localite.json', json_encode($result));
-
-    if ($creation == false) {
-      echo "Probleme lors de la creation du fichier json";
-        }else{
-      echo "Fichier json creer avec succes";
-    }
-    // Affichage de l'erreur si elle a été trouvée.
-   
   } catch (Exception $e) {
     echo 'ERREUR: ' . $e->getMessage();
   }
